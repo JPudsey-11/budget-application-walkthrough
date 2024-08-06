e819e50bb879696bde693f77a723f1326d2cfa17
Jamie Pudsey <jamiepudsey@gmail.com>
2024-08-06 07:03:23 +0000

    updating views and models

--- budget_application_app/views.py
+++ budget_application_app/views.py
@@ -1,10 +1,12 @@
 # budget_application_app/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import projects
 
 def project_list(request):
     return render(request, 'budget/project-list.html')
 
 def project_detail(request, project_slug):
+    project = get_object_or_404(Project, slug=project_slug)
     # fetch the correct project
-    return render(request, 'budget/project-detail.html')
+    return render(request, 'budget/project-detail.html', {'project': project, 'expense_list': project.expenses})
