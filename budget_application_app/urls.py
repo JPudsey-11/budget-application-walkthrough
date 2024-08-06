81881be3b7f8c6639d05a4dbec150dbdf5104f42
Jamie Pudsey <jamiepudsey@gmail.com>
2024-08-06 08:39:56 +0000

    updated views and urls.py

--- budget_application_app/urls.py
+++ budget_application_app/urls.py
@@ -1,8 +1,9 @@
-# budget_application_app/urls.py
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.project_list, name='list'),
-    path('<slug:project_slug>/', views.project_detail, name='detail'),  # Ensure the slash at the end to avoid ambiguity
+    path('add', views.ProjectCreateView.as_view(), name='add'),
+    path('<slug:project_slug>', views.project_detail, name='detail')
 ]
--- budget_application_app/views.py
+++ budget_application_app/views.py
@@ -1,12 +1,15 @@
-# budget_application_app/views.py
-
 from django.shortcuts import render, get_object_or_404
-from .models import projects
+from .models import Project
+from django.views.generic import CreateView
 
 def project_list(request):
     return render(request, 'budget/project-list.html')
 
 def project_detail(request, project_slug):
     project = get_object_or_404(Project, slug=project_slug)
-    # fetch the correct project
-    return render(request, 'budget/project-detail.html', {'project': project, 'expense_list': project.expenses})
+    return render(request, 'budget/project-detail.html', {'project': project, 'expense_list': project.expenses.all()})
+
+class ProjectCreateView(CreateView):
+    model = Project
+    template_name = 'budget/add-project.html'
+    fields = ('name', 'budget')
